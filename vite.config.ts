e315062cec901980d2ffcf8b2cb5c5b747eb191c
梁志鹏 <liangzp02@vanke.com>
2022-11-14 16:25:16 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,38 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Daily Day',
+        short_name: 'D D',
+        icons: [
+          {
+            src: 'icon144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
       '@comp': resolve(__dirname, 'src/components'),
     },
+  },
+  server: {
+    host: '0.0.0.0',
   },
   build: {
     outDir: 'dist',
